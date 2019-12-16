007dbc540f598ec68f60dfbc161787f0e89e3bec
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2019-12-15 22:58:58 -0600

    Updates

---
+++ precache-manifest.a64eed2570fa2a314ad6d0003e8829ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de970cf64a9ac1e35be743f1645b8ea",
+    "url": "/Ramdom-Quote-Machine/index.html"
+  },
+  {
+    "revision": "bf9406525a9ed3d63879",
+    "url": "/Ramdom-Quote-Machine/static/css/main.d199fa3f.chunk.css"
+  },
+  {
+    "revision": "316fe34f780067e66c1e",
+    "url": "/Ramdom-Quote-Machine/static/js/2.662d7688.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Ramdom-Quote-Machine/static/js/2.662d7688.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf9406525a9ed3d63879",
+    "url": "/Ramdom-Quote-Machine/static/js/main.a5fd7133.chunk.js"
+  },
+  {
+    "revision": "93ad7c6cf3fb32c5817c",
+    "url": "/Ramdom-Quote-Machine/static/js/runtime-main.37cad3fb.js"
+  }
+]);
